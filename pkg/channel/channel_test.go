57f18b7b6768a5089b8cc33f3833226544d18d53
channing duan <channing.duan@gmail.com>
2023-01-15 14:55:11 +0800

    delete move testing code
    Signed-off-by: channing duan <channing.duan@gmail.com>
    Signed-off-by: Channing Duan <phpythoner@gmail.com>

--- pkg/channel/channel_test.go
+++ pkg/channel/channel_test.go
@@ -1,15 +1,12 @@
 package channel_test
 
 import (
-	"fmt"
 	. "github.com/onsi/ginkgo/v2"
 )
 
 var _ = Describe("Channel", func() {
 	PIt("Create new channel", func() {})
-	PIt("Update channel", func() {
-		fmt.Println("11111")
-	})
+	PIt("Update channel", func() {})
 	PIt("Peer join channel", func() {})
 	PIt("Peer exit channel", func() {})
 	PIt("List channel", func() {})
